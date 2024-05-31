bae6c7f12851125c0aec31474bdbf2fdc4c7cfd9
JosePabloSG <suarezgomezjosepablo03@gmail.com>
2024-05-30 23:14:45 -0600

    chore: Add appointment state to AppointmentsContext

--- citas-clinica-frontend/src/context/AppointmentsProvider.tsx
+++ citas-clinica-frontend/src/context/AppointmentsProvider.tsx
@@ -1,12 +1,16 @@
 import { ReactNode, useState } from "react"
 import AppointmentsContext from "./AppointmentsContext"
+import { Appointment } from "@/types/Appointments"
 
 const AppointmentsProvider = ({ children }: { children: ReactNode }) => {
     const [newAppointmentCreated, setNewAppointmentCreated] = useState(false)
+    const [appointment, setAppointment] = useState<Appointment>({} as Appointment)
     return (
         <AppointmentsContext.Provider value={{
             newAppointmentCreated,
-            setNewAppointmentCreated
+            setNewAppointmentCreated,
+            appointment,
+            setAppointment
         }}>
             {children}
         </AppointmentsContext.Provider>
