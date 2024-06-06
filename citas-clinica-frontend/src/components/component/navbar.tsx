4913cdd319dcd9da51121c5b9a27332832779aeb
Nazagomez <nazasanchez12.26@gmail.com>
2024-06-06 15:34:16 -0600

    Logo

--- citas-clinica-frontend/src/components/component/navbar.tsx
+++ citas-clinica-frontend/src/components/component/navbar.tsx
@@ -10,7 +10,9 @@ export function Navbar() {
   return (
     <header className="flex h-16 items-center justify-between bg-[#134e4a] px-4 md:px-6">
       <Link className="text-lg font-bold text-white" to={"/"}>
-        Greys Clinic
+      <img src="LogoImages\Logo.jpeg" alt="Greys Clinic Logo" className="h-12 mr-3" />
+        <span className="text-2x1">Grey's Clinic</span>
+        
       </Link>
       <div className="flex items-center my-5">
         {tokenData.Role === 'ADMIN' && (
